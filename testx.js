2e2dd19c5d80970804352a542a23d2f5534ac898
Nhzi <emmanuelaajayi2@gmail.com>
2024-10-18 15:34:18 -0400

    Tried node on Testx.js

---
+++ testx.js
@@ -0,0 +1,12 @@
+function fnc() {
+    console.log('Hello, World!');
+    console.log('This is a simple JavaScript function');
+    console.log('The function has been executed successfully');
+    console.log('The function will now terminate');
+    return 8;
+}
+
+fnc();
+const typer = fnc();
+
+console.log(typer);
